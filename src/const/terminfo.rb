13fc922bafdda5759de014bd59d1976bd89b8262
b1tt <maxst8bit@gmail.com>
2020-07-01 22:02:52 +0300

    deleted tests, updated readme, .gitkeep

--- src/const/terminfo.rb
+++ src/const/terminfo.rb
@@ -4,8 +4,7 @@
 require "io/console"
 
 module Gemsheru module Const module Terminal
-  @size = {
-# 0: columns, 1: rows
+  @size = { # 0: columns, 1: rows
     "x" => IO.console.winsize[1],
     "y" => IO.console.winsize[0]
   }
@@ -14,6 +13,3 @@ module Gemsheru module Const module Terminal
     @size
   end
 end             end          end
-
-puts "X: #{Gemsheru::Const::Terminal.size["x"]}"
-puts "Y: #{Gemsheru::Const::Terminal.size["y"]}"
